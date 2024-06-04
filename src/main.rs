6be5012be02d0a2d615c5e2ae4cbd7f3e556f6ce
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-06-04 10:42:12 +0200

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn handle_connection(mut stream: TcpStream) {
     // iter over the buffer then get the first line
     // more likely the header
     // e.g Request: "GET / HTTP/1.1"
-    let http_request = buffer.lines().next().unwrap().unwrap();
+    let request_line = buffer.lines().next().unwrap().unwrap();
 
     // A Closer Look at an HTTP Request
     //HTTP is a text-based protocol, and a request takes this format:
@@ -55,7 +55,7 @@ fn handle_connection(mut stream: TcpStream) {
     //e.g simple response HTTP/1.1 200 OK\r\n\r\n
     //
     // writing a simple http response with no body
-    let status = "HTTP/1.1 200 OK";
+    // let status = "HTTP/1.1 200 OK";
 
     // return the reponse to the client
     // convert it to bytes, tcp transport is a byte stream protocol
@@ -67,19 +67,19 @@ fn handle_connection(mut stream: TcpStream) {
     //
     // TODO: Validating the request and selectively responding
     //
-
+    //
+    /* ### OLD CODE
     if http_request.starts_with("GET / HTTP/1.1") {
         let contents = fs::read_to_string("hello.html").unwrap();
         let length = contents.len();
 
-        // format the response
+        # format the response
         let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-        // render the content to the browser on each request
+        # render the content to the browser on each request
         stream.write(response.as_bytes()).unwrap();
     } else {
-        // return the 404.html Not Found
-
+        # return the 404.html Not Found
         let contents = fs::read_to_string("404.html").unwrap();
 
         let length = contents.len();
@@ -89,4 +89,21 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write(reponse.as_bytes()).unwrap();
     }
+    */
+
+    // Touch of refactoring
+    // remove the code repetition
+    // clean up
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }

8786b235446b8192900d166e7ba4818eedecad25
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-06-10 23:16:12 +0200

    single threaded into multiple threaded

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
     // Creating Space to Store the threads
     // threads: Vec<thread::JoinHandle<()>>,
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 // our ThreadPool have a new and execute methods
 //
@@ -13,20 +17,28 @@ pub struct ThreadPool {
 //efine a Worker::new function that takes an id number and returns a Worker instance that holds the id and a thread spawned with an empty closure.
 //In ThreadPool::new, use the for loop counter to generate an id, create a new Worker with that id, and store the worker in the vector.
 
+// struct Job;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         // validatinf the number of threads in new
         // using the assert;
-
+        // TODO: Sending Requests to Threads via Channels
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let reciever = Arc::new(Mutex::new(receiver));
+
         // let mut threads = Vec::with_capacity(size);
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&reciever)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     // the execute method should me the same as the thread spawn
@@ -37,6 +49,9 @@ impl ThreadPool {
         // inherit the thread::spawn properties
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -46,8 +61,14 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
+        });
 
         Worker { id, thread }
     }
